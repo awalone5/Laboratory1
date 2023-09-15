ea8ac21561bea135be4080344ea57beb2f5d6a2b
admin <admin@LAPTOP-K8P8B68V>
2023-09-15 14:28:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//int main()
+//{
+// TASK 18
+// 
+//	int a;
+//	int min = 2147483647;
+//	int max = -2147483648;
+//	bool isExit = true;
+//	while (isExit)
+//	{
+//		cin >> a;
+//		if (a != 0)
+//		{
+//			if (a < min)
+//			{
+//				min = a;
+//			}
+//			else if (a > max)
+//			{
+//				max = a;
+//			}
+//		}
+//		else if (a == 0)
+//		{
+//			cout << min << " " << max;
+//			isExit = false;
+//		}
+//	}
+//}
+
+//int main() {
+// TASK 27
+//
+//	for (int i = 1; i <= 1000; i++)
+//	{
+//		if (i % 5 == 0 && i % 2 != 0)
+//		{
+//			cout << i << endl;
+//		}
+//	}
+//}
+
+int main() {
+// TASK 9
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	double plus = -b / a;
+	double multiply = c / a;
+
+	double result1 = plus + sqrt(plus * plus - 4 * multiply) / 2;
+	double result2 = plus - sqrt(plus * plus - 4 * multiply) / 2;
+
+	cout << result1 << " " << result2 << endl;
+}
